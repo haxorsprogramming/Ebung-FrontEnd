26882a93a1239a3158e831e21c57b7a83abf24ce
haxorsprogramming <haxorsuinsu@gmail.com>
2020-12-03 10:09:39 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
+use function PHPSTORM_META\map;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,6 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // Kategori produk
         DB::table('tbl_kategori_produk') -> insert([
             'kd_kategori' => 'BUNGA',
             'nama_kategori' => 'Bunga',
@@ -38,5 +41,25 @@ class DatabaseSeeder extends Seeder
             'deksripsi' => 'Kategori cake',
             'active' => '1'
         ]);
+        // Sub kategori 
+        DB::table('tbl_sub_kategori') -> insert([
+            'kd_sub_kategori' => 'BUNGASEGAR',
+            'nama_kategori' => 'Bunga Segar',
+            'kd_kategori' => 'BUNGA',
+            'active' => '1'
+        ]);
+        DB::table('tbl_sub_kategori') -> insert([
+            'kd_sub_kategori' => 'BUNGAPLASTIK',
+            'nama_kategori' => 'Bunga Plastik',
+            'kd_kategori' => 'BUNGA',
+            'active' => '1'
+        ]);
+        DB::table('tbl_sub_kategori') -> insert([
+            'kd_sub_kategori' => 'BOUQET',
+            'nama_kategori' => 'Bunga BOUQUET',
+            'kd_kategori' => 'BUNGA',
+            'active' => '1'
+        ]);
+        
     }
 }
